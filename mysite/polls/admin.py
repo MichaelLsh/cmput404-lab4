60df85fb7a179eabb0ea6123a0f3b2961adfb507
MichaelLsh <shihao8@DESKTOP-JKGE86U.localdomain>
2023-01-30 15:39:50 -0700

    lab4 Using Django Admin part completed

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 # Let the admin interact with an app
 # Register your models here.
+# from django.contrib import admin
+
+from .models import Choice, Question
+
+admin.site.register(Choice)
+admin.site.register(Question)
